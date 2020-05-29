56a152d14313fd01bb679ed2c1ef07f3b2bcba67
Ishani Santurkar <ishani.santurkar@gmail.com>
2020-05-28 18:08:58 -0700

    Add pause and play for slideshow, add highlighting of current tab

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,6 +18,8 @@ const loc = "/images/life/";
 let currImageNum = 1;
 let sliderSpeed = 5000;
 let myTimer = setInterval(loopOverImages, sliderSpeed);
+let paused = false;
+const headerElems = ["#about", "#skills", "#edu", "#gallery"];
 
 // Function that shows or hides the list of courses
 // for a particular semester upon user click
@@ -38,47 +40,93 @@ function toggleCourseList(listObject) {
 // function that changes slideshow's picture to the next 
 // picture when the user presses the next button
 function next() {
-    clearInterval(myTimer);
+    if(!paused)
+    {
+        clearInterval(myTimer);
+    }
     currImageNum++;
     if(currImageNum === 17) {
         currImageNum = 1;
     }
     const newImgPath = loc + currImageNum + ".jpg";
     document.getElementById("galleryimg").src = newImgPath;
-    myTimer = setInterval(loopOverImages, sliderSpeed);
+    if(!paused)
+    {
+        myTimer = setInterval(loopOverImages, sliderSpeed);
+    }
 }
 
 // function that changes slideshow's picture to the previous 
 // picture when the user presses the previous button
 function prev() {
-    clearInterval(myTimer);
+    if(!paused)
+    {
+        clearInterval(myTimer);
+    }
     currImageNum--;
     if(currImageNum === 0) {
         currImageNum = 16;
     }
     const newImgPath = loc + currImageNum + ".jpg";
     document.getElementById("galleryimg").src = newImgPath;
-    myTimer = setInterval(loopOverImages, sliderSpeed);
+    if(!paused)
+    {
+        myTimer = setInterval(loopOverImages, sliderSpeed);
+    }
 }
 
 // function that is called every sliderSpeed seconds
 // changes slideshow to display next image in list of 
 // images
 function loopOverImages() {
-    currImageNum++;
-    if(currImageNum === 17) {
-        currImageNum = 1;
+    if(!paused) {
+        currImageNum++;
+        if(currImageNum === 17) {
+            currImageNum = 1;
+        }
+        const newImgPath = loc + currImageNum + ".jpg";
+        document.getElementById("galleryimg").src = newImgPath;
+        console.log(currImageNum + " " + document.getElementById("galleryimg").style.width);
     }
-    const newImgPath = loc + currImageNum + ".jpg";
-    document.getElementById("galleryimg").src = newImgPath;
 }
-
 
 // modifies slider speed if user changes value
 // of slider on gallery page
 function sliderMoved() {
-    clearInterval(myTimer);
+    if(!paused)
+    {
+        clearInterval(myTimer);
+    }
     const sliderValue = document.getElementById("galleryslider").value;
     sliderSpeed = sliderValue * 1000;
-    myTimer = setInterval(loopOverImages, sliderSpeed);
+    if(!paused)
+    {
+        myTimer = setInterval(loopOverImages, sliderSpeed);
+    }
 }
+
+function togglePause() {
+    if(paused) {
+        const statusImg = document.getElementById("pauseplay")
+        statusImg.src = "/images/play.png";
+        statusImg.style.display = "block";
+        window.setTimeout(function () {
+            $("#pauseplay").fadeOut();
+            statusImg.style.display = "none";
+        }, 250);
+        paused = false;
+        myTimer = setInterval(loopOverImages, sliderSpeed);
+    }
+    else {
+        const statusImg = document.getElementById("pauseplay")
+        statusImg.src = "/images/pause.png";
+        statusImg.style.display = "block";
+        window.setTimeout(function () {
+            $("#pauseplay").fadeOut();
+            statusImg.style.display = "none";
+        }, 250);
+        paused = true;
+        clearInterval(myTimer);
+    }
+}
+
